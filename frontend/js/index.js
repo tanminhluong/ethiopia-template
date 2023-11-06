ffb8ce587231b84f582eb217e8a9b566c345488d
Minh Tan <tanluong@mobcec.com>
2023-11-06 14:07:26 +0700

    push code

---
+++ frontend/js/index.js
@@ -0,0 +1,26 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./frontend/_js/main.js ***!
+  \******************************/
+const menuIcon = document.querySelector(".menu-mobile--icon");
+const navMobile = document.querySelector(".header__nav--mobile");
+const navOverlay = document.querySelector(".nav-overlay");
+
+const handleClick = (e) => {
+  if (navMobile.style.transform === "translateX(-100%)") {
+    navMobile.style.transform = "translateX(0)";
+    navMobile.style.transition = "all ease-in 0.2s";
+    navOverlay.style.display = "block";
+  } else {
+    navMobile.style.transform = "translateX(-100%)";
+    navOverlay.style.display = "none";
+  }
+};
+
+navOverlay.addEventListener("click", handleClick);
+menuIcon.addEventListener("click", handleClick);
+
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
