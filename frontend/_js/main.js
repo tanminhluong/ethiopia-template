16c0594a31f772318868fbfd5b953ebcc2d005e7
Minh Tan <tanluong@mobcec.com>
2023-11-08 10:53:44 +0700

    change show mobile navbar method

--- frontend/_js/main.js
+++ frontend/_js/main.js
@@ -2,20 +2,6 @@ const menuIcon = document.querySelector(".menu-mobile--icon");
 const navMobile = document.querySelector(".header__nav--mobile");
 const navOverlay = document.querySelector(".nav-overlay");
 
-const handleClick = (e) => {
-  if (navMobile.style.transform === "translateX(-100%)") {
-    navMobile.style.transform = "translateX(0)";
-    navMobile.style.transition = "all ease-in 0.2s";
-    navOverlay.style.display = "block";
-  } else {
-    navMobile.style.transform = "translateX(-100%)";
-    navOverlay.style.display = "none";
-  }
-};
-
-navOverlay.addEventListener("click", handleClick);
-menuIcon.addEventListener("click", handleClick);
-
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
